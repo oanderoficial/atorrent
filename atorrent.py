7eb599a916d332839fcbf93bc9f6f2ec2edc1f5e
Anderson B Silva (Ander) <32654298+oanderoficial@users.noreply.github.com>
2023-12-30 18:23:23 -0300

    Update atorrent.py
    Foi definido funções para facilitar a manutenção e adição de funcionalidades no futuro.

--- atorrent.py
+++ atorrent.py
@@ -1,6 +1,15 @@
 import os
 from colorama import Fore, Style
 
+def download_torrent(link):
+    os.system(f"aria2c {link}")
+
+def download_bittorrent(link):
+    os.system(f"aria2c {link}")
+
+def stream_torrent(link):
+    os.system(f"webtorrent {link} 0 1")
+
 while True:
     print(Fore.GREEN + """
                                       
@@ -22,19 +31,19 @@ while True:
     if opcao == "1":
         print(Fore.RED + "-----------------------------\n" + Style.RESET_ALL)
         link = input(Fore.GREEN + "Digite o link magnético: " + Style.RESET_ALL)
-        os.system(f"aria2c {link}")
+        download_torrent(link)
         print(Fore.RED + "-----------------------------\n" + Style.RESET_ALL)
 
     elif opcao == "2":
         print(Fore.RED + "-----------------------------\n" + Style.RESET_ALL)
-        bittorrent = input(Fore.GREEN + "Digite o link BitTorrent: " + Style.RESET_ALL)
-        os.system(f"aria2c {bittorrent}")
+        link = input(Fore.GREEN + "Digite o link BitTorrent: " + Style.RESET_ALL)
+        download_bittorrent(link)
         print(Fore.RED + "-----------------------------\n" + Style.RESET_ALL)
 
     elif opcao == "3":
         print(Fore.RED + "-----------------------------\n" + Style.RESET_ALL)
-        streaming = input(Fore.GREEN + "Digite o link magnético: " + Style.RESET_ALL)
-        os.system(f"webtorrent {streaming} 0 1")
+        link = input(Fore.GREEN + "Digite o link magnético: " + Style.RESET_ALL)
+        stream_torrent(link)
         print(Fore.RED + "-----------------------------\n" + Style.RESET_ALL)
 
     elif opcao == "0":
